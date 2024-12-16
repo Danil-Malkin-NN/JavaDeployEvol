4f7f7a3d1d1a93cde3fea1b9e19b7192d18fe53e
dvmalkin <dvmalkin@phenixit.ru>
2024-12-16 10:54:44 +0300

    init commit

---
+++ src/main/java/ru/nino/deploy/JavaDeployEvolApplication.java
@@ -0,0 +1,13 @@
+package ru.nino.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDeployEvolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDeployEvolApplication.class, args);
+	}
+
+}

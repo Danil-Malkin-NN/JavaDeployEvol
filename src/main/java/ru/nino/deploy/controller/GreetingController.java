e02823c2d2ae71d460b50b8ca2d3630ecd18adf6
dvmalkin <dvmalkin@phenixit.ru>
2024-12-18 10:34:09 +0300

    чуть красота

--- src/main/java/ru/nino/deploy/controller/GreetingController.java
+++ src/main/java/ru/nino/deploy/controller/GreetingController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    @GetMapping()
-    public String sayHello() {
-        return "Hello World";
-    }
+	@GetMapping()
+	public String sayHello() {
+		return "Hello World";
+	}
 
 }
